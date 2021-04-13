9936ec1ff8622bc30ec44d84b96c8f6f6a2f2343
Benjamin Byeon <bb3037@columbia.edu>
2021-04-13 05:18:13 +0000

    Fix management commands; import and export squirrel data

--- sightings/management/commands/export_squirrel_data.py
+++ sightings/management/commands/export_squirrel_data.py
@@ -1,16 +1,21 @@
 from django.core.management.base import BaseCommand, CommandError
 from sightings.models import Sightings
 import csv, os, sys
-from django.db.models import get_model
+from django.apps import apps
+
 class Command(BaseCommand):
         help = "Export data to .csv file"
-        args = '[squirrel_tracker.Sightings]'
+        
+        def add_arguments(self, parser):
+            parser.add_argument('path', type=str)
+
         def handle(self,*app_labels,**kwargs):
-                app_name, model_name = app_labels[0].split('.')
-                model = get_model('sightings','Sightings')
-                field_names = [f.name for f in model._meta.fields]
-                writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL)
-                writer.writerow(field_names)
-                for instance in model.objects.all():
-                        writer.writerow([unicode(getattr(instance, f)).encode('utf-8') for f in field_names])
+        
+            path_to_file = kwargs['path']
+            model = apps.get_model('sightings','Sightings')
+            field_names = [f.name for f in model._meta.fields]
+            writer = csv.writer(sys.stdout, quoting=csv.QUOTE_ALL)
+            writer.writerow(field_names)
+            for instance in model.objects.all():
+                    writer.writerow([(getattr(instance, f)) for f in field_names])
 
--- sightings/management/commands/import_squirrel_data.py
+++ sightings/management/commands/import_squirrel_data.py
@@ -1,69 +1,57 @@
 from django.core.management.base import BaseCommand, CommandError
 from sightings.models import Sightings
-import csv
-import os
+import csv, os
+import datetime
+
+
 class Command(BaseCommand):
-        help = "Read data - note: non-mandatory fields have been assigned arbitrary features"
-        
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.model_name = Sightings
 
-        def add_arg(self, parser):
-                parser.add_argument('path_to_file', type = str)
-        def handle(self, *args, **options):
-                path = options['path_to_file']
+    help = "Read data - note: non-mandatory fields have been assigned arbitrary features"
+    
+    def __enter__(self):
+         return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.finalize()
+
+    def add_arguments(self, parser):
+        parser.add_argument('path', type=str)
+    
+    def handle(self, *args, **kwargs):
+        path_to_file = kwargs['path']
+        with open(path_to_file, 'r') as csvFile:
+            reader = csv.reader(csvFile)
+            next(reader)
+            Sightings.objects.all().delete()
+            for i in list(reader):
                 try:
-                        with open(path, 'r') as csv_file:
-                                csv_reader = csv.reader(csv_file)
-                                for row in csv_reader:
-                                    if row[7] is not None:
-                                        age = row[7]
-                                    else:
-                                        age = "Juvenile"
-                                    if row[8] is not None:
-                                        primary = row[8]
-                                    else:
-                                        primary = "Gray"
-                                    if row[12] is not None:
-                                        location=row[12]
-                                    else:
-                                        location ="Ground Plane"
-                                    if row[14] is not None:
-                                        specificlocation = row[14]
-                                    else:
-                                        specificlocation = "on tree stump"
-                                    
-                                    if row[20] is not None:
-                                        otheractivities=row[20]
-                                    else:
-                                        otheractivities="grooming"
-
-                                    added_sighting=Sightings(
-                                                Latitude =float(row[0]),
-                                                Longitude = float(row[1]),
-                                                UniqueSquirrelID=row[2],
-                                                Shift = row[4],
-                                                Date = f"{row[5][4:]}-{row[5][:2]}-{row[5][2:4]}",
-                                                Age = age,
-                                                PrimaryFurColor=primary,
-                                                Location=location,
-                                                SpecificLocation=specificlocation,
-                                                Running=row[15].lower(),
-                                                Chasing=row[16].lower(),
-                                                Climbing=row[17].lower(),
-                                                Eating=row[18].lower(),
-                                                Foraging=row[19].lower(),
-                                                OtherActivities=otheractivities,
-                                                Kuks=row[21].lower(),
-                                                Quaas=row[22].lower(),
-                                                Moans=row[23].lower(),
-                                                TailFlags=row[24].lower(),
-                                                TailTwitches=row[25].lower(),
-                                                Approaches=row[26].lower(),
-                                                Indifferent=row[27].lower(),
-                                                RunsFrom=row[28].lower(),
-                                                )
-                                    added_sightings.save()
+                    #date =  i[5][4:] + '-' + i[5][0:2] + '-' + i[5][2:4]
+                    added_sightings = Sightings(
+                            Latitude = float(i[1]),
+                            Longitude = float(i[0]),
+                            UniqueSquirrelID = i[2],
+                            Shift = i[4],
+                            Date = f"{i[5][4:]}-{i[5][:2]}-{i[5][2:4]}",
+                            Age = i[7],
+                            Primary_Fur_Color = i[8],
+                            Location = i[12],
+                            SpecificLocation = i[14],
+                            Running = str(i[15]).lower(),
+                            Chasing = str(i[16]).lower(),
+                            Climbing = str(i[17]).lower(),
+                            Eating = str(i[18]).lower(),
+                            Foraging = str(i[19]).lower(),
+                            OtherActivities = i[20],
+                            Kuks = str(i[21]).lower(),
+                            Quaas = str(i[22]).lower(),
+                            Moans = str(i[23]).lower(),
+                            TailFlags = str(i[24]).lower(),
+                            TailTwitches = str(i[25]).lower(),
+                            Approaches = str(i[26]).lower(),
+                            Indifferent = str(i[27]).lower(),
+                            RunsFrom = str(i[28]).lower(),
+                    )   
+                    added_sightings.save()
                 except:
-                    return "Sorry, something went wrong"
+                    return "Sorry, something went nuts!"
+

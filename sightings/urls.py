0ad95dcd7251f2a1569e471bcf2f438fe0388e76
Matt Hamati <mnh2128@columbia.edu>
2021-04-13 06:01:53 +0000

    Update index html for body

--- sightings/urls.py
+++ sightings/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-        path('', views.index,name='index'),
+        path('', views.index, name='index'),
         path('add/', views.AddSquirrel, name ='add'),
 #        path('<unique_squirrel_id>/', views.UpdateSquirrel, name = 'update'),
         path('stats/', views.Stats, name = 'stat'),

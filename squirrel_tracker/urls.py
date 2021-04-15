0bbfb0e81b8d6b6c85d6d41bd823ac636894a61c
Matt Hamati <mnh2128@columbia.edu>
2021-04-15 06:36:39 +0000

    Fix html files & linking issues between pages

--- squirrel_tracker/urls.py
+++ squirrel_tracker/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('map/', include('map.urls')),
+    path('map/', include('map.urls'), name='map'),
     path('sightings/',include('sightings.urls')),
 ]
